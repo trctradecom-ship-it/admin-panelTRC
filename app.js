39e0577d6bc3789d37ef5c0cd5cdeeb1f01b2588
Tradecom <trctradecom@gmail.com>
2026-05-12 17:52:35 +0530

    Fix tokenAddress to correct value

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let provider, signer, user, contract, token, chart;
 
 const contractAddress = "0xCcC20c59bA3827f6E40B7Debc60a804836c55740";
-const tokenAddress = "0xCcC20c59bA3827f6E40B7Debc60a804836c55740";
+const tokenAddress = "0xCcC20c59bA3827f6E40B7Debc60a804836c557400";
 
 const abi = [
     "function owner() view returns(address)",
